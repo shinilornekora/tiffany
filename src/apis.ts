73334f35db84e6bac963797fc25e14d7f26fc5b5
Shiniasse <arokenrol@mail.ru>
2023-09-28 15:06:10 +0300

    profile page

---
+++ src/apis.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const apis = {
+    user: {
+        list: axios.get('http://localhost:8080/api/users'),
+        active: axios.get('http://localhost:8080/api/users/active'),
+        inactive: axios.get('http://localhost:8080/api/users/inactive'),
+    },
+    product: {
+        list: axios.get('http//localhost:8080/api/products'),
+        licenseKeys: axios.get('http://localhost:8080/api/products/licenses'),
+    },
+    payments: {
+        list: axios.get('http://localhost:8080/api/payments'),
+    },
+    forum: {
+        messages: axios.get('http://localhost:8080/api/forum'),
+    }
+}
+
+export default apis;
--- src/shared/pages/Profile/Profile.tsx
+++ src/shared/pages/Profile/Profile.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
-
+import { useStyles } from './styles';
+import Header from '../../components/header';
+import cardIcon from '../../static/profile/card.png'
+import noAvatar from '../../static/profile/no-avatar.png'
 const Profile = () => {
-    return (
-        <div>
 
-        </div>
+    const { classes } = useStyles();
+
+    return (
+        <>
+            <Header/>
+            <div className={classes.profileContent}>
+                <div className={classes.profileContentWrapper}>
+                    <img src={noAvatar} alt="check" className="avatar"/>
+                    <div className={classes.profileContentInfo}>
+                        <div className={classes.profileContentInfoBlock}>
+                            <div className={classes.profileContentInfoBlockName}>
+                                Анатолий
+                            </div>
+                            <div className={classes.profileContentInfoBlockEmail}>
+                                example@mail.ru
+                            </div>
+                            <div className={classes.profileContentInfoBlockCreditCard}>
+                                <img src={cardIcon} alt="check"/>
+                                <p>****5538</p>
+                            </div>
+                        </div>
+                        <div className={classes.profileContentInfoBlock}>
+                            <div onClick={() => {}}>View purchased products</div>
+                            <div onClick={() => {}}>Change account info</div>
+                            <div onClick={() => {}}>Log out</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
