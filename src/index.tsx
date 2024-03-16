2096e9cfd459b04e4a12e2c6184bdf5adf5fe35d
dimitrich212003 <ytrewq212003@mail.ru>
2024-03-16 17:07:04 +0300

    add authorization

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// @ts-expect-error: тайпскрипт не умеет так
+window.authToken = '';
+
+// @ts-expect-error: тайпскрипт не умеет так
+window['Tiffany'] = {
+	info: () => {
+		return 'Hi! My name is Tiffany. It stands for The Entropy Frontend';
+	},
+	logAsAdmin: () => {
+		// @ts-expect-error: тайпскрипт не умеет так
+		window.authToken = 'superuser777';
+
+		return 'Now you are logged as Admin. Bless you!';
+	}
+};
+
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
